4c220a3e14b049c0af83673e34bb738160cae0f8
TiaErikaDev <erikamayarapimentel@gmail.com>
2022-04-22 11:39:27 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -5,10 +5,14 @@ import model.Gato;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
-
         System.out.println(gato);
+
+        Livro livro1 = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(livro1);
+
 /*
         System.out.println("Hello World!");
 */
     }
 }
+
